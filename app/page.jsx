19487c1c5737a80f9597a5041e8afb3cbef99d2f
Faizan Mansuri <126244804+DevFaizann@users.noreply.github.com>
2023-07-17 23:54:29 +0530

    working with components

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,23 @@
+import Feed from "@components/Feed";
+
+
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share 
+        <br className="max-md:hidden"/>
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        PromptAI is an open-source AI prompting tool for modern world to discover, 
+        create and share creative prompts
+      </p>
+
+      {/* Feed */}
+    </section>
   )
 }
 
