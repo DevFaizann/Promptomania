2729facfb373729121c6c6b15cd38b361857e02b
Faizan Mansuri <126244804+DevFaizann@users.noreply.github.com>
2023-07-29 17:22:44 +0530

    github and google auth WIP

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github"
 
+import User from '@models/user';
 import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
@@ -9,9 +11,20 @@ const handler = NextAuth({
             clientId:process.env.GOOGLE_ID,
             clientSecret:process.env.GOOGLE_CLIENT_SECRET,
         })
+        // ,
+        // GithubProvider({
+        //     clientId:process.env.GITHUB_ID,
+        //     clientSecret:process.env.GITHUB_CLIENT_SECRET,
+        // })
     ],
     async session({session}){
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        })
 
+        session.user.id = sessionUser._id.toString();
+
+        return session;
     },
 
     async signIn({profile}){
@@ -20,8 +33,19 @@ const handler = NextAuth({
             await connectToDB();
     
             //check if a user already exists
+            const userExists = await User.findOne({
+                email: profile.email
+            });
     
             //if not, create a user
+
+            if(!userExists){
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image:profile.picture                    
+                })
+            }
             
             return true;
         } catch (error) {
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,25 +6,25 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
-  const [toggleDropdown, setToggleDropdown] = useState(false)
+  const [toggleDropdown, setToggleDropdown] = useState(false);
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     };
 
-    setProviders();
+    setUpProviders();
   }, []);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image
           src="/assets/images/logo.svg"
-          alt="PromptAI Logo"
+          alt="Logo"
           width={30}
           height={30}
           className="object-contain"
@@ -34,7 +34,7 @@ const Nav = () => {
 
       {/* Desktop navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -46,7 +46,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -61,7 +61,9 @@ const Nav = () => {
                 <button
                   type="button"
                   key={provider.name}
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
                   className="black_btn"
                 >
                   Sign In
@@ -73,10 +75,10 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
@@ -85,36 +87,34 @@ const Nav = () => {
             />
 
             {toggleDropdown && (
-                <div className="dropdown">
-                    <Link
-                        href="/profile"    
-                        className="dropdown_link"
-                        onClick={() => setToggleDropdown(false)}
-                    >
-                        My Profile
-                    </Link>
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
 
-                    <Link
-                        href="/create-prompt"    
-                        className="dropdown_link"
-                        onClick={() => setToggleDropdown(false)}
-                    >
-                        Create Prompt
-                    </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
 
-                    <button
-                        type="button"
-                        onClick={() => {
-                            setToggleDropdown(false)
-                            signOut();
-                        }}
-                        className="mt-5 w-full black_btn"
-                    >
-                        Sign Out
-
-                    </button>
-
-                </div>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
             )}
           </div>
         ) : (
@@ -124,7 +124,9 @@ const Nav = () => {
                 <button
                   type="button"
                   key={provider.name}
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
                   className="black_btn"
                 >
                   Sign In
